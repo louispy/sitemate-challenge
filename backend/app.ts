8f2b0075b050834dc68ee237d01474c25597e723
Louis <louis.leslie@sicepat.com>
2024-09-02 08:25:47 +0700

    Add get issues api
    The API returns an array of mock issues

--- backend/app.ts
+++ backend/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { Issue } from "./models/issue";
+import { mockIssue } from "./mocks/mockIssue";
 
 const app = express();
 const port = 5000;
@@ -10,10 +11,15 @@ app.get("/", (req, res) => {
   res.send("Hello, TypeScript Node Express!");
 });
 
-app.post("/issues", (req, res) => {
+app.post("/issue", (req, res) => {
   const issue: Issue = req.body;
   console.log(issue);
   res.status(200).json({ message: "Success", data: issue });
+});
+
+app.get("/issue", (req, res) => {
+  console.log([mockIssue]);
+  res.status(200).json({ message: "Success", data: [mockIssue] });
 });
 
 app.listen(port, () => {
---
+++ backend/mocks/mockIssue.ts
@@ -0,0 +1,7 @@
+import { Issue } from "../models/issue";
+
+export const mockIssue: Issue = {
+  id: "1",
+  title: "mock issue",
+  description: "mock description",
+};
