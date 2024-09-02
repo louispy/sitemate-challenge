a0ff677541e3dbc788dac2f0857bc7a0cbe1ed43
Louis <louis.leslie@sicepat.com>
2024-09-02 08:45:15 +0700

    Add mock functionalities for issues

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { Issue } from "./models/issue";
-import { mockIssue } from "./mocks/mockIssue";
 
 const app = express();
 const port = 5000;
@@ -11,26 +10,43 @@ app.get("/", (req, res) => {
   res.send("Hello, TypeScript Node Express!");
 });
 
+let issues: Issue[] = [];
+
 app.post("/issue", (req, res) => {
   const issue: Issue = req.body;
   console.log(issue);
+  issues.push(issue);
   res.status(200).json({ message: "Success Create Issue", data: issue });
 });
 
 app.get("/issue", (req, res) => {
-  console.log([mockIssue]);
-  res.status(200).json({ message: "Success Get Issues", data: [mockIssue] });
+  res.status(200).json({ message: "Success Get Issues", data: issues });
 });
 
 app.patch("/issue/:id", (req, res) => {
-  console.log('to be updated id:', req.params.id);
+  console.log("to be updated id:", req.params.id);
   const issue: Issue = req.body;
+  for (let i = 0; i < issues.length; i++) {
+    if (issues[i].id == req.params.id) {
+      issues[i] = issue;
+      res.status(200).json({ message: "Success Updated", data: issue });
+      return;
+    }
+  }
   console.log(issue);
-  res.status(200).json({ message: "Success Updated", data: issue });
+  res.status(404).json({ message: "Data Not Found" });
 });
 
 app.delete("/issue/:id", (req, res) => {
-  console.log('to be deleted id:', req.params.id);
+  console.log("to be deleted id:", req.params.id);
+  const result = [];
+
+  for (let i = 0; i < issues.length; i++) {
+    if (issues[i].id != req.params.id) {
+      result.push(issues[i]);
+    }
+  }
+  issues = result;
   res.status(200).json({ message: "Success Delete Issue", data: { id: req.params.id } });
 });
 
