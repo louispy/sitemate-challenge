d59691a261d6a5f12e52b191e1e59aeb21e153ba
Louis <louis.leslie@sicepat.com>
2024-09-02 08:21:51 +0700

    Add create issue api

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import { Issue } from "./models/issue";
 
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello, TypeScript Node Express!");
+});
+
+app.post("/issues", (req, res) => {
+  const issue: Issue = req.body;
+  console.log(issue);
+  res.status(200).json({ message: "Success", data: issue });
 });
 
 app.listen(port, () => {
